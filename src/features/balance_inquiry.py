d8fd1488ee7ab7d348dd6a8fb0f3641f1ea142cb
MercyKemboi <kemboimercy49@gmail.com>
2024-12-18 14:59:12 +0300

    fix: added commas in numbers

--- src/features/balance_inquiry.py
+++ src/features/balance_inquiry.py
@@ -1,3 +1,3 @@
 #function for checking the balance and displaying to the user
 def check_balance(account_balance):
-    print(f"\nYour current account balance is: Ksh {account_balance:.2f}")
+    print(f"\nYour current account balance is: Ksh {account_balance:,.2f}")
