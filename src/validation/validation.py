a69285644c76d0935e320da96c5fe0773c53ea54
MercyKemboi <kemboimercy49@gmail.com>
2024-12-18 10:24:55 +0300

    feat: Added the input validaton functionality

---
+++ src/validation/validation.py
@@ -0,0 +1,12 @@
+def input_validation(inputChoice):
+    try:
+        #type casting input choice to integer
+        inputChoice = int(inputChoice)
+        #check if the input is between 1 and 4
+        if 1 <= inputChoice <= 4:
+            return inputChoice
+        else:
+            print("Invalid option! Please choose a valid option (1-4).")
+    except ValueError:
+        print("Invalid input! Please enter a numeric value.")
+    return None
